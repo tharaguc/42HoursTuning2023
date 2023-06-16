8b103adb286a37ca8d79929b4ff64b80bb2aafa5
tsk.haraguchi@gmail.com <tsk.haraguchi@gmail.com>
2023-06-17 00:38:40 +0900

    nginx loadbalance

--- app/node/src/index.ts
+++ app/node/src/index.ts
@@ -1,6 +1,13 @@
 import { app } from "./app";
 
-const port = 8000;
-app.listen(port, () => {
-  console.log(`ポート${port}番で起動しました。`);
+// const port = 8000;
+// app.listen(port, () => {
+//   console.log(`ポート${port}番で起動しました。`);
+// });
+
+const ports = [8000, 8001];
+ports.forEach(port => {
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
 });
