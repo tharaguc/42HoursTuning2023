d6653328bcda42e5c01ded558031bdac75ae507a
grifis <nekogrifis@gmail.com>
2023-06-17 18:06:37 +0900

    index追加

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -2,4 +2,6 @@ ALTER TABLE match_group_member ADD INDEX user_id_idx(user_id);
 ALTER TABLE department_role_member ADD INDEX user_id_idx(user_id, belong);
 ALTER TABLE `user` ADD `id` INT UNSIGNED NOT NULL AUTO_INCREMENT FIRST, ADD UNIQUE (`id`);
 CREATE INDEX idx_role_id_belong ON department_role_member(role_id, belong);
+CREATE INDEX idx_mail_password ON user (mail, password);
+
 
